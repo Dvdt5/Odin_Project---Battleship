55ea80418bdfa9b352c56f8570158b5a346cc1f1
Davut Dagtekin <dagtekindavut07@gmail.com>
2025-01-18 13:28:03 +0300

    now can rotate and place ships

--- src/DOMHandler.js
+++ src/DOMHandler.js
@@ -7,11 +7,20 @@ export class DOMHandler {
 
 
     openShipLayoutBoard(board) {
+        const currShip = player.gameBoard.ships.filter(ship => ship.isPlaced == false)[0] || null;
 
+        if (currShip === null){
+            this.confirmLayoutPage(board);
+            return;
+        }
 
         if (document.querySelector("#player-input-page")) {
             document.getElementById("player-input-page").remove();
         }
+        if (document.querySelector("#example-ship")) {
+            document.getElementById("example-ship").remove();
+        }
+        
 
         const main = document.getElementById("content");
         
@@ -19,27 +28,47 @@ export class DOMHandler {
         const layoutPage = document.createElement("div");
         layoutPage.id = "player-input-page";
         
+        const exampleShip = document.createElement("div");
+        exampleShip.id = "example-ship";
+        if (currShip.isVertical){
+            exampleShip.classList.add("column");
+            exampleShip.classList.remove("row");
+        }
+        main.appendChild(exampleShip);
+        exampleShip.classList.add("row");
 
         const shipInfoText = document.createElement("h2");
         shipInfoText.id = "ship-info-text";
 
-        const currShip = [];
-        player.gameBoard.ships.forEach(ship => {
-            if (!ship.isPlaced){
-                currShip.push(ship.name);
+        
+        if (!exampleShip.hasChildNodes()) {
+            for (let i = 0; i < currShip.size; i++){
+                exampleShip.appendChild(document.createElement("div"));
             }
-            shipInfoText.textContent = `Place your ${currShip[0] ? currShip[0]:"hi"} `;
-        });
+        }
+
+        shipInfoText.textContent = `Place your ${currShip.name ? currShip.name:"hi"} `;
 
         const rotateShipBtn = document.createElement("i");
         rotateShipBtn.classList.add("fa");
         rotateShipBtn.classList.add("fa-repeat");
+        rotateShipBtn.style.cursor = "pointer";
+
+        rotateShipBtn.addEventListener("click", ()=>{
+            const tempShip = document.getElementById("example-ship");
+            player.gameBoard.rotateShip(currShip.name);
+            tempShip.classList.toggle("row");
+            tempShip.classList.toggle("column");
+            
+        });
+        
 
         shipInfoText.appendChild(rotateShipBtn);
         layoutPage.appendChild(shipInfoText);
         layoutPage.appendChild(this.placeShipsBoard(board));
 
         main.appendChild(layoutPage)
+        
     }
 
     closeStartScreen(){
@@ -48,6 +77,38 @@ export class DOMHandler {
         startPage.remove();
 
         this.openShipLayoutBoard(player.gameBoard.board);
+    }
+
+    confirmLayoutPage(board){
+        if (document.querySelector("#player-input-page")) {
+            document.getElementById("player-input-page").remove();
+        }
+        if (document.querySelector("#example-ship")) {
+            document.getElementById("example-ship").remove();
+        }
+        
+
+        const main = document.getElementById("content");
+        
+
+        const layoutPage = document.createElement("div");
+        layoutPage.id = "player-input-page";
+        
+        
+
+        const shipInfoText = document.createElement("h2");
+        shipInfoText.id = "ship-info-text";
+
+        shipInfoText.textContent = `Confirm your Layout`;
+
+        
+        layoutPage.appendChild(shipInfoText);
+
+        const boardToConfirm = this.placeShipsBoard(board);
+        boardToConfirm.style.pointerEvents = "none";
+        layoutPage.appendChild(boardToConfirm);
+
+        main.appendChild(layoutPage)
     }
 
     renderGameBoard(board){
@@ -96,14 +157,19 @@ export class DOMHandler {
                 const boardCell = document.createElement("div");
                 boardCell.classList.add("cell");
                 boardCell.addEventListener("click", ()=> {
-                    player.gameBoard.ships.forEach(ship => {
-                        if (!ship.isPlaced){
-                            player.gameBoard.addShip(ship.name, col,row)
-                        }
-                    });
+                    const currShip = player.gameBoard.ships.filter(ship => ship.isPlaced == false)[0];
+                    player.gameBoard.addShip(currShip.name, col,row)
                     this.openShipLayoutBoard(player.gameBoard.board);
                     }
                 );
+
+                boardCell.addEventListener("mouseover", ()=>{
+                    boardCell.classList.add("hoveredCell");     
+                });
+
+                boardCell.addEventListener("mouseout", ()=>{
+                    boardCell.classList.remove("hoveredCell");
+                });
 
                 if (board[row][col] === 0){
                     boardCell.classList.add("undiscovered");
